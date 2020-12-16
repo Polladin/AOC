d8c5168ee838552f4cdd69ee74b8e8525f74fe6b
Alexey Kukushkin <>
2020-12-16 20:01:14 +0300

    Day 13

---
+++ 2020/src/day_13/day_13.cpp
@@ -0,0 +1,18 @@
+//
+// Created by alexpall on 16.12.2020.
+//
+
+#include <iostream>
+
+#include "shuttle_search.h"
+
+
+int main()
+{
+  ShuttleSearch shuttleSearch;
+
+//  std::cout << "Res test 1: " << shuttleSearch.task_1( INPUT_FILES "/day_13/test_1.txt" ) << std::endl;
+//  std::cout << "Res TASK 1: " << shuttleSearch.task_1( INPUT_FILES "/day_13/input_1.txt" ) << std::endl;
+
+  std::cout << "Res test 1: " << shuttleSearch.task_2( INPUT_FILES "/day_13/test_1.txt" ) << std::endl;
+}
---
+++ 2020/src/day_13/shuttle_search.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include <string>
+
+
+class ShuttleSearch
+{
+public:
+    int task_1( const std::string & filename );
+
+    long long task_2( const std::string & filename );
+};
