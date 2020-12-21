83965a29ad170d6a64bfb4f6a5bc77f72e042d00
Alexey Kukushkin <Alexey.Kukushkin@nordigy.ru>
2020-12-21 17:16:11 +0300

    Day 15

--- 2020/src/day_15/day_15.cpp
+++ 2020/src/day_15/day_15.cpp
@@ -2,3 +2,17 @@
 // Created by alexpall on 20.12.2020.
 //
 
+
+#include <iostream>
+
+#include "rambunctious_recitation.h"
+
+
+int main()
+{
+  RambunctiousRecitation rambunctiousRecitation;
+
+  std::cout << "Res test 1: " << rambunctiousRecitation.task_1( { 0, 3, 6 } ) << std::endl;
+  std::cout << "Res TASK 1: " << rambunctiousRecitation.task_1( { 8, 0, 17, 4, 1, 12 } ) << std::endl;
+
+}
--- 2020/src/day_15/rambunctious_recitation.cpp
+++ 2020/src/day_15/rambunctious_recitation.cpp
@@ -5,6 +5,8 @@
 #include "rambunctious_recitation.h"
 
 #include <map>
+#include <unordered_map>
+#include <iostream>
 
 #include "common/file_reader.h"
 
@@ -12,12 +14,37 @@
 
 int RambunctiousRecitation::task_1( const std::vector< int > & inputNumbers  )
 {
-  std::map< int, int > lastSpokenNumbers;
+  std::unordered_map< int, int > lastSpokenNumbers;
 
-  for ( std::size_t idx = 0; idx < inputNumbers.size(); ++idx )
+  for ( std::size_t idx = 0; idx < inputNumbers.size() - 1; ++idx )
   {
     lastSpokenNumbers[ inputNumbers[ idx ] ] = idx + 1;
   }
 
-  
+  int lastNumber = inputNumbers.back();
+  int currentStep = inputNumbers.size();
+
+  while ( currentStep < 30000000 )
+  {
+//    std::cout << "Last number: " << lastNumber << " step: " << currentStep << std::endl;
+
+    auto it = lastSpokenNumbers.find( lastNumber );
+
+    // Number not found
+    if ( it == lastSpokenNumbers.end() )
+    {
+      lastSpokenNumbers[ lastNumber ] = currentStep;
+      ++currentStep;
+      lastNumber = 0;
+      continue;
+    }
+
+    // Found
+    int newNumber = currentStep - it->second;
+    lastSpokenNumbers[ lastNumber ] = currentStep;
+    ++currentStep;
+    lastNumber = newNumber;
+  }
+
+  return lastNumber;
 }
