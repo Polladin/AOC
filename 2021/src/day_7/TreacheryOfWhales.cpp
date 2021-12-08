c70adffd62cb1509ea13d478b8fa44c171856ae9
Alexey Kukushkin <Alexey.Kukushkin@nordigy.ru>
2021-12-08 12:37:38 +0300

    2021 Day 7

---
+++ 2021/src/day_7/TreacheryOfWhales.cpp
@@ -0,0 +1,82 @@
+//
+// Created by alexey on 07.12.2021.
+//
+
+#include "TreacheryOfWhales.h"
+
+#include <algorithm>
+#include <iostream>
+
+
+int TreacheryOfWhales::task_1( const std::vector< int > & crabs )
+{
+  int maxCrab = *( std::max_element( crabs.begin(), crabs.end() ) );
+  int minCrab = *( std::min_element( crabs.begin(), crabs.end() ) );
+
+//  std::cout << "Min: " << minCrab << " Max: " << maxCrab << "\n";
+
+  std::vector< int > fuel ( maxCrab + 1, std::numeric_limits< int >::max() );
+
+  for ( int _idx = minCrab; _idx <= maxCrab; ++_idx )
+  {
+    fuel[ _idx ] = 0;
+    for ( auto _crab : crabs )
+    {
+      fuel[ _idx ] += std::abs( _crab - _idx );
+    }
+//    std::cout << "Idx: " << _idx << " fuel: " << fuel[ _idx ] << "\n";
+  }
+
+  int min = std::numeric_limits< int >::max();
+  std::size_t pos = 0;
+
+  for ( std::size_t _idx = minCrab; _idx <= maxCrab; ++_idx )
+  {
+    if ( min > fuel[ _idx ] )
+    {
+      pos = _idx;
+      min = fuel[ _idx ];
+    }
+  }
+
+  return fuel[ pos ];
+}
+
+int get_arifmetic_seq( int num )
+{
+  return ( 1 + num ) * num / 2;
+}
+
+int TreacheryOfWhales::task_2( const std::vector<int> &crabs )
+{
+  int maxCrab = *( std::max_element( crabs.begin(), crabs.end() ) );
+  int minCrab = *( std::min_element( crabs.begin(), crabs.end() ) );
+
+//  std::cout << "Min: " << minCrab << " Max: " << maxCrab << "\n";
+
+  std::vector< int > fuel ( maxCrab + 1, std::numeric_limits< int >::max() );
+
+  for ( int _idx = minCrab; _idx <= maxCrab; ++_idx )
+  {
+    fuel[ _idx ] = 0;
+    for ( auto _crab : crabs )
+    {
+      fuel[ _idx ] += get_arifmetic_seq( std::abs( _crab - _idx ) ) ;
+    }
+//    std::cout << "Idx: " << _idx << " fuel: " << fuel[ _idx ] << "\n";
+  }
+
+  int min = std::numeric_limits< int >::max();
+  std::size_t pos = 0;
+
+  for ( std::size_t _idx = minCrab; _idx <= maxCrab; ++_idx )
+  {
+    if ( min > fuel[ _idx ] )
+    {
+      pos = _idx;
+      min = fuel[ _idx ];
+    }
+  }
+
+  return fuel[ pos ];
+}
---
+++ 2021/src/day_7/TreacheryOfWhales.h
@@ -0,0 +1,19 @@
+//
+// Created by alexey on 07.12.2021.
+//
+
+#ifndef ADVENTOFCODE2021_TREACHERYOFWHALES_H
+#define ADVENTOFCODE2021_TREACHERYOFWHALES_H
+
+#include <vector>
+
+
+class TreacheryOfWhales
+{
+public:
+  int task_1( const std::vector< int > & crabs );
+  int task_2( const std::vector< int > & crabs );
+};
+
+
+#endif //ADVENTOFCODE2021_TREACHERYOFWHALES_H
